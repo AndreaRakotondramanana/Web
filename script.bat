227fd0d6dd569c6cd77c792eb743063d1196260b
AndreaRakotondramanana <andrearakotondramanana0@gmail.com>
2023-07-07 18:55:31 +0300

    Sprint8bis2

---
+++ script.bat
@@ -0,0 +1,14 @@
+cd framework/src
+javac -parameters -d . *.java
+jar -cvf ../../etu1831.jar etu1831
+copy ../../etu1831.jar ../../test-framework/WEB-INF/lib/
+
+set CLASSPATH=.;D:/Sprint/WebDynamique/Sprint8-bis-2/framework/etu1831.jar
+
+cd ../../test-framework/WEB-INF/classes
+javac -parameters -d . *.java
+cd ../../
+jar -cvf ../test-framework.war .
+
+cd ../
+pause
