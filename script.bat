4897c03e81a7ef6a4bf87ab779d5b7fe383857c6
AndreaRakotondramanana <andrearakotondramanana0@gmail.com>
2023-07-07 18:46:03 +0300

    Sprint9

--- script.bat
+++ script.bat
@@ -1,9 +1,9 @@
 cd framework/src
 javac -parameters -d . *.java
-jar -cvf ../../etu1831.jar etu1831
-copy ../../etu1831.jar ../../test-framework/WEB-INF/lib/
+jar -cvf ../../etu1767.jar etu1767
+copy ../../etu1767.jar ../../test-framework/WEB-INF/lib/
 
-set CLASSPATH=.;D:/Sprint/WebDynamique/Sprint8-2/framework/etu1831.jar
+set CLASSPATH=.;D:/Sprint/WebDynamique/Sprint9-2/framework/etu1831.jar
 
 cd ../../test-framework/WEB-INF/classes
 javac -parameters -d . *.java
